c5d71be07b1d844d5cac2e27a8107d70a6e31e21
Bruno Ranieri <bruno_ranieri@hotmail.com>
2016-12-09 00:39:08 -0200

    Arrumando tabela

--- codingrace1/application/views/curso/cursos_view.php
+++ codingrace1/application/views/curso/cursos_view.php
@@ -7,7 +7,7 @@
 
     <?php $this->load->view('commons/menupagina')?>
 
-    <div class="w3-container">
+    <div class="w3-container w3-row-padding w3-margin-bottom">
         <table class="w3-table-all">
             <thead>
             <tr class="w3-light-grey">
