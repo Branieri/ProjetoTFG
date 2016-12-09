ce723e2315a5f7d472e106f11bdd8f21416c71e7
Bruno Ranieri <bruno_ranieri@hotmail.com>
2016-12-09 02:10:05 -0200

    Tentando ajustar tabela final2

--- codingrace1/application/views/curso/cursos_view.php
+++ codingrace1/application/views/curso/cursos_view.php
@@ -8,7 +8,7 @@
             <?php $this->load->view('commons/menupagina')?>
 
             <div class="w3-container">
-                <table class="w3-table-all w3-container">
+                <table class="w3-table-all">
                     <thead>
                         <tr class="w3-light-grey">
                             <th>Nome</th>
