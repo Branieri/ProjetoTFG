97f459a69cfe2dd468cc7d9aa37ff13b5dc59d8c
Bruno Ranieri <bruno_ranieri@hotmail.com>
2016-12-09 02:00:18 -0200

    Tentando ajustar tabela

--- codingrace1/application/views/commons/menupagina.php
+++ codingrace1/application/views/commons/menupagina.php
@@ -4,7 +4,7 @@
         </header>
 
         <div class="w3-row-padding w3-margin-bottom">
-            <div class="w3-quarter">
+            <!--<div class="w3-quarter">-->
                 <div class="w3-container w3-red w3-padding-16">
                     <div class="w3-left"><i class="fa fa-comment w3-xxxlarge"></i></div>
                     <div class="w3-right">
@@ -12,7 +12,7 @@
                     </div>
                     <div class="w3-clear"></div>
                     <h4>Messages</h4>
-                </div>
+                <!--</div>-->
             </div>
             <div class="w3-quarter">
                 <div class="w3-container w3-blue w3-padding-16">
